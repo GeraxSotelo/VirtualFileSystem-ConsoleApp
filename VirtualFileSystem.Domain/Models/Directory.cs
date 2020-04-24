c053f819a7c8d99600ddb350491186bd071b2d69
Gerax Sotelo <gsotelo23@gmail.com>
2020-04-24 16:58:38 -0600

    Create Directory & File classes

---
+++ VirtualFileSystem.Domain/Models/Directory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualFileSystem.Domain
+{
+    public class Directory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ParentDirectoryId { get; set; }
+    }
+}
